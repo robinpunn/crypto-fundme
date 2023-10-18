cbe2d2272f77bc7a00c6105aa5e79aafd1da0ce5
Robin Punnoose <robin.punnoose@protonmail.com>
2023-10-18 06:19:13 -0500

    added state variables, modifier, and constuctor to FundMe.sol

--- backend/src/FundMe.sol
+++ backend/src/FundMe.sol
@@ -1,21 +1,58 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-// import OZ onlyowner??
+/**
+    set up a contract factory.
+
+    user should be able to launch a new contract and become owner
+
+    create a mapping that will track donors and amounts
+
+    contract should accept donations
+
+    owner can cancel to return funds
+        create a cancel bool so contract can't be withdrawn from
+
+    only owner should be able to withdraw
+        use self destruct ???
+
+    import OZ onlyowner??
+ */
 
 contract FundMe {
-    // set up a contract factory.
+    error FundeMe__NotOwner();
 
-    // user should be able to launch a new contract and become owner
+    /**
+    * @notice funders key mapped to amount funded
+    * @dev mapping updated anytime a user funds contract
+    */
+    mapping(address => uint256) private s_addressFundedAmount;
+    /**
+    * @notice array of addresses that have funded the account
+    * @dev this array should be updated any time a new user funds contract
+    */
+    address[] private s_funders;
+    /**
+    * @notice contract owner
+    */
+    address payable private immutable i_owner;
 
-    // create a mapping that will track donors and amounts
+    /**
+    * @notice Checks if msg.sender is owner
+    */
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) {
+            revert FundeMe__NotOwner();
+        }
+        _;
+    }
 
-    // contract should accept donations
+    /**
+    * @notice sets i_owner to msg.sender
+    */
+    constructor() {
+        i_owner = payable(msg.sender);
+    }
 
-    // owner can cancel to return funds
-        // create a cancel bool so contract can't be withdrawn from
-
-    // only owner should be able to withdraw
-        // use self destruct ???
 
 }
