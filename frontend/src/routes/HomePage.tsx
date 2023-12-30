12de7fefcdc3e76463e097e1465452c5f020824f
Robin Punnoose <robin.punnoose@protonmail.com>
2023-12-30 15:27:16 -0600

    added content to homepage

--- frontend/src/routes/HomePage.tsx
+++ frontend/src/routes/HomePage.tsx
@@ -2,7 +2,26 @@ import styles from '../styles/Home.module.css'
 
 function HomePage() {
     return (
-        <section className={styles.homePage}>THIS IS THE HOMEPAGE</section>
+        <section className={styles.homePage}>
+            <article className={styles.intro}>
+                <div>
+                    This app allows users to participate in crowd funding using the Ethereum blockchain. Currently deployed on the Sepolia testnet, users have the option to create contracts and donate Eth to contracts.
+                </div>
+            </article>
+
+            <article className={styles.info}>
+                <div className={styles.create}>
+                    <h2>CREATE</h2>
+                    <p>Create a contract and become its owner</p>
+                    <p>Withdraw at any time</p>
+                </div>
+                <div className={styles.donate}>
+                    <h2>DONATE</h2>
+                    <p>Donate Eth to any contract</p>
+                    <p>Browse contracts </p>
+                </div>
+            </article>
+        </section>
     )
 }
 
